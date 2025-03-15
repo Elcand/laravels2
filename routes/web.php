c6c8692ad6628ca393467b866332e5306448c61f
Elcand <tjandraraharja99@gmail.com>
2025-03-15 14:29:08 +0700

    belajar join - inner join

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/join', function () {
+    $userWithOrders = DB::table('users')
+        ->join('orders', 'users.id', '=', 'orders.user_id')
+        ->select('users.*', 'orders.*')
+        ->get();
+
+    dd($userWithOrders);
 });
