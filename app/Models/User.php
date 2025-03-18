87c8eb7a9a0a101373eea58ddd5f8500b2e3d27f
Elcand <tjandraraharja99@gmail.com>
2025-03-18 15:48:56 +0700

    belajar Eloquent Relations - one to one

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Psy\CodeCleaner\ReturnTypePass;
 
 class User extends Authenticatable
 {
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    function address()
+    {
+        return $this->hasOne(Address::class, 'user_id', 'id');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -7,27 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/join', function () {
-    // $userWithOrders = DB::table('users') //inner join
-    //     ->join('orders', 'users.id', '=', 'orders.user_id')
-    //     ->select('users.*', 'orders.*')
-    //     ->get();
-    // $userWithOrders = DB::table('users') // left join
-    //     ->leftJoin('orders', 'users.id', '=', 'orders.user_id')
-    //     ->select('users.name', 'orders.product_name')
-    //     ->get();
-    // $orderWithOrders = DB::table('orders') // right join
-    //     ->rightJoin('users', 'users.id', '=', 'orders.user_id')
-    //     ->select('orders.product_name', 'users.name')
-    //     ->get();
-    // $fullOuterJoin = DB::table('users')
-    //     ->leftJoin('orders', 'orders.id', '=', 'orders.user_id')
-    //     ->select('users.name', 'orders.product_name')
-    //     ->unionall(
-    //         DB::table('users')
-    //             ->rightJoin('orders', 'orders.user_id', '=', 'orders.user_id')
-    //             ->select('users.name', 'orders.product_name')
-    //     )->get();
-
-    // dd($fullOuterJoin);
+Route::get('/relation', function () {
+    $users = User::all();
+    return view('test', compact('users'));
 });
